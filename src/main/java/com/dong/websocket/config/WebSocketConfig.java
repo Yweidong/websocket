dc969e127893893752c1351da722c762831fc293
wei_tung <1371690483@qq.com>
2020-08-18 20:03:41 +0800

    websocket

--- src/main/java/com/dong/websocket/config/WebSocketConfig.java
+++ src/main/java/com/dong/websocket/config/WebSocketConfig.java
@@ -2,6 +2,7 @@ package com.dong.websocket.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 import org.springframework.web.socket.server.standard.ServerEndpointExporter;
@@ -13,6 +14,7 @@ import org.springframework.web.socket.server.standard.ServerEndpointExporter;
  * @create: 2020-08-18 15:52
  **/
 @Configuration
+
 public class WebSocketConfig {
     @Bean
     public ServerEndpointExporter serverEndpointExporter() {
--- src/main/java/com/dong/websocket/server/WebSocketServer.java
+++ src/main/java/com/dong/websocket/server/WebSocketServer.java
@@ -1,13 +1,13 @@
 package com.dong.websocket.server;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import javax.websocket.*;
 import javax.websocket.server.PathParam;
 import javax.websocket.server.ServerEndpoint;
 import java.io.IOException;
-import java.util.Map;
-import java.util.Set;
 import java.util.concurrent.CopyOnWriteArraySet;
 
 /**
@@ -18,11 +18,13 @@ import java.util.concurrent.CopyOnWriteArraySet;
  **/
 @ServerEndpoint("/websocket/{userId}")
 @Component
-public class webSocketServer {
+
+public class WebSocketServer {
+    private static final Logger logger =LoggerFactory.getLogger(WebSocketServer.class);
     //静态变量，用来记录当前在线连接数。应该把它设计成线程安全的。
     private static int onlineCount = 0;
     //concurrent包的线程安全Set，用来存放每个客户端对应的MyWebSocket对象。
-    private static CopyOnWriteArraySet<webSocketServer> webSocketSet = new CopyOnWriteArraySet<webSocketServer>();
+    private static CopyOnWriteArraySet<WebSocketServer> webSocketSet = new CopyOnWriteArraySet<WebSocketServer>();
 
     //与某个客户端的连接会话，需要通过它来给客户端发送数据
     private Session session;
@@ -43,12 +45,16 @@ public class webSocketServer {
         addOnlineCount();
         webSocketSet.add(this);
 
+
+
         try {
-            sendMessage("连接成功");
+            sendMessage("连接成功1111");
+                logger.info("【websocket消息】有新的连接, 总数:{"+webSocketSet.size()+"}");
 
         }
         catch (IOException e){
-//            logger.info(userId+"上线的时候通知所有人发生了错误");
+            logger.info("websocket IO异常");
+
         }
 
 
@@ -61,20 +67,23 @@ public class webSocketServer {
     public void onClose() {
         webSocketSet.remove(this);  //从set中删除
         subOnlineCount();           //在线数减1
+        logger.info("【websocket消息】连接断开, 总数:{"+webSocketSet.size()+"}");
     }
 
     @OnError
     public void onError(Session session, Throwable error) {
-//        log.error("发生错误");
+
+        logger.info("发生错误");
         error.printStackTrace();
     }
 
 
     @OnMessage
     public void onMessage(String message, Session session) {
-//        log.info("收到来自窗口"+sid+"的信息:"+message);
+        logger.info("收到来自窗口"+userId+"的信息:"+message);
+
         //群发消息
-        for (webSocketServer item : webSocketSet) {
+        for (WebSocketServer item : webSocketSet) {
             try {
                 item.sendMessage(message);
             } catch (IOException e) {
@@ -85,7 +94,7 @@ public class webSocketServer {
 
     public static void sendInfo(String message,@PathParam("sid") String userId) throws IOException {
 //        log.info("推送消息到窗口"+sid+"，推送内容:"+message);
-        for (webSocketServer item : webSocketSet) {
+        for (WebSocketServer item : webSocketSet) {
             try {
                 //这里可以设定只推送给这个sid的，为null则全部推送
                 if (userId == null) {
@@ -108,10 +117,10 @@ public class webSocketServer {
     }
 
     private static synchronized void addOnlineCount() {
-        webSocketServer.onlineCount++;
+        WebSocketServer.onlineCount++;
     }
     private static synchronized void subOnlineCount() {
-        webSocketServer.onlineCount--;
+        WebSocketServer.onlineCount--;
     }
 
 }
